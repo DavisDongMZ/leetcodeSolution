c223e5962f17839abc212664bb3319e5d91785b6
DavisDongMZ <mingzhi.dong@mail.utoronto.ca>
2024-09-13 05:51:05 -0400

    closure question

---
+++ day1/helloWorld.js
@@ -0,0 +1,18 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+/**
+Some thoughts: This question is just asking return hello world,
+but it's good chance to review closure
+**/
